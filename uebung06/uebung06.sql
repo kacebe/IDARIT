0f28c217bd6110aef5f237d15a3476ede29a02a9
Kai-Christian Bruhn <bruhn@fh-mainz.de>
2014-12-23 23:55:46 +0100

    Update uebung06.sql

--- uebung06/uebung06.sql
+++ uebung06/uebung06.sql
@@ -4,7 +4,7 @@ IDARIT 15.12.2014
 Kai-Christian Bruhn
 Hochschule Mainz
 
-Daten: 
+Daten: https://github.com/kacebe/IDARIT/raw/master/uebung06/ausgangsdaten/atlantgis_uebung06.sqlite (4.9 MByte)
 */
 -- Grundlegender Aufbau der Abfrage
 SELECT asites.ID_AR_SITE, s.cat, asites.geom, s.geom FROM archaeological_sites AS asites, streams AS s;
@@ -13,23 +13,23 @@ SELECT asites.ID_AR_SITE, s.cat, asites.geom, s.geom FROM archaeological_sites A
 SELECT asites.ID_AR_SITE, s.cat, ST_Distance(asites.geom, s.geom)
 FROM archaeological_sites AS asites, streams AS s;
 
---
+-- Beschränkung auf minimalen Wert und Aggregation nach Archaeological Sites
 SELECT asites.ID_AR_SITE, s.cat, min(ST_Distance(asites.geom, s.geom))
 FROM archaeological_sites AS asites, streams AS s
 GROUP BY asites.ID_AR_SITE;
 
--- 
+-- Mit ST_ShortestLine wird die Distanzlinie als Geometrie erzeugt
 SELECT asites.ID_AR_SITE, s.cat, ST_ShortestLine(asites.geom, s.geom) AS geom_line_abstand, min(ST_Distance(asites.geom, s.geom)) AS MinDistanz
 FROM archaeological_sites AS asites, streams AS s
 GROUP BY asites.ID_AR_SITE;
 
--- 
+-- Tabellenerstellungsabfrage
 CREATE TABLE gewaesserdistanz AS 
 SELECT asites.ID_AR_SITE, s.cat, ST_ShortestLine(asites.geom, s.geom) AS geom_line_abstand, min(ST_Distance(asites.geom, s.geom)) AS MinDistanz
 FROM archaeological_sites AS asites, streams AS s
 GROUP BY asites.ID_AR_SITE;
 
--- 
+-- Geometriespalte aktivieren
 SELECT RecoverGeometryColumn( "gewaesserdistanz" , "geom_line_abstand" , 32628 , "LINESTRING" , 2 );
 
 
